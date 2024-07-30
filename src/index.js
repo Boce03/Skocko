d8e620c2d9502c0108570ff6e50833b553d22823
Boce03 <bogdanmicic03vr@gmail.com>
2024-07-30 22:06:00 +0200

    added new directory

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const images = ['../images/Pik.png', '../images/Karo.png', '../images/Skocko.png', 
+                '../images/Tref.png', '../images/Srce.png', '../images/Zvezda.png'];
+
+const symbols = new Map();
+symbols.set('pik', 0);
+symbols.set('karo', 1);
+symbols.set('skocko', 2);
+symbols.set('tref', 3);
+symbols.set('srce', 4);
+symbols.set('zvezda', 5);
