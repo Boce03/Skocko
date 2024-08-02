797013452dea1c08b4bbcc9a1d87e40e6afb7de1
Boce03 <bogdanmicic03vr@gmail.com>
2024-08-02 23:57:17 +0200

    fixed active state of fields and buttons

--- src/gamestate.js
+++ src/gamestate.js
@@ -1,5 +1,7 @@
 import logic from './logic';
 import timer from './timer';
+import event from './event';
+import action from './actions';
 
 const numOfFld = 4;
 const numOfSym = 6;
@@ -8,22 +10,57 @@ let komb = [];
 let filled = [];
 let row;
 
+let btnStartGame = document.getElementById('start');
+
 const resetState = function(){
     komb = [];
     filled = new Array(numOfFld).fill(false);
     row = 0;
     logic.generate();
-
+    
+    
     let timerDiv = document.querySelector('.timer-inner');
     timerDiv.style.height = '0';
-
     timer.init(gameDuration, timerDiv);
     timer.start();
+
+    
+    event.addEvent(function(){
+        return document.querySelectorAll('div.right-bottom-container .btn');
+    }, action.add, 'click');
+
+    event.addEvent(function(){
+        return document.querySelectorAll('div#fld-0 div.fld-hover');
+    }, action.remove, 'click');
+
+    event.addEvent(function(){
+        return document.querySelectorAll('div#fld-0 .btn');
+    }, action.submit, 'click');
+
+    event.removeEvent(function(){
+        return document.querySelectorAll('#start');
+    }, resetState, 'click');
 }
 
 const nextRow = function(){
+    event.removeEvent(function(){
+        return document.querySelectorAll(`div#fld-${row} div.fld-hover`);
+    }, action.remove, 'click');
+
+    event.removeEvent(function(){
+        return document.querySelectorAll(`div#fld-${row} .btn`);
+    }, action.submit, 'click');
+    
     row++;
     filled.fill(false);
+
+    event.addEvent(function(){
+        return document.querySelectorAll(`div#fld-${row} div.fld-hover`);
+    }, action.remove, 'click');
+
+    event.addEvent(function(){
+        return document.querySelectorAll(`div#fld-${row} .btn`);
+    }, action.submit, 'click');
 }
 
 export default {
--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import event from './event';
 
 console.log("uspesno ucitano");
 
-gameState.resetState();
-console.log(gameState);
+//gameState.resetState();
 
-event.addEvent(function(){
+
+/*event.addEvents(function(){
     return document.querySelectorAll('div.right-bottom-container .btn');
 }, action.add, 'click');
 
-event.addEvent(function(){
+event.addEvents(function(){
     return document.querySelectorAll('div.left-container div.fld-hover');
 }, action.remove, 'click');
 
-event.addEvent(function(){
+event.addEvents(function(){
     return document.querySelectorAll('div.left-row .btn');
-}, action.submit, 'click');
+}, action.submit, 'click');*/
+
+let btnStartGame = document.getElementById('start');
+btnStartGame.addEventListener('click', gameState.resetState);
+console.log(gameState);
